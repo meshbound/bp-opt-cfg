005395382cdc05b3db5f15205ed4be75573d6e08
Jack Weigl <jackcweigl@gmail.com>
2024-11-16 15:04:26 -0600

    Cleanup and progress on state

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pymunk
 import random
 import math
 
-ball_colors = {
+BALL_COLORS = {
     1 : (255, 205, 0),
     2 : (0, 0, 205),
     3 : (205, 0, 0),
@@ -11,7 +11,6 @@ ball_colors = {
     5 : (255, 102, 0),
     6 : (0, 128, 0),
     7 : (128, 0, 0),
-    8 : (0, 0, 0),
     9 : (255, 205, 0),
     10 : (0, 0, 205),
     11 : (205, 0, 0),
@@ -21,174 +20,287 @@ ball_colors = {
     15 : (128, 0, 0)
 }
 
-d_width, d_height = 600, 400
-t_width, t_height = 478, 232
-                     
-pocket_radius = 16
-t_corner = pocket_radius * 1.1
-t_mid = pocket_radius * 2
+# table
+WINDOW_WIDTH, WINDOW_HEIGHT = 600, 400
+TABLE_WIDTH, TABLE_HEIGHT = 478, 232
+       
+POCKET_RADIUS = 16
+TABLE_CORNER = POCKET_RADIUS
+TABLE_MID = POCKET_RADIUS * 2
 
-bezel = pocket_radius * 2
+TABLE_BEZEL = POCKET_RADIUS * 2
+MARK_RADIUS = 3 
+TRIM_RADIUS = 2
 
-ball_radius = 7
+# ball
+BALL_RADIUS = 7
+
+P1_BOUNDS = ((BALL_RADIUS, BALL_RADIUS), (3*TABLE_WIDTH//8, TABLE_HEIGHT - BALL_RADIUS))
+P2_BOUNDS = ((5*TABLE_WIDTH//8, BALL_RADIUS), (TABLE_WIDTH - BALL_RADIUS, TABLE_HEIGHT - BALL_RADIUS))
+
+# sim
+SIM_SPEED = 1
+FPS=60
 
 pygame.init()
-display = pygame.display.set_mode((d_width, d_height))
-clock = pygame.time.Clock()
-space = pymunk.Space()
 
 font = pygame.font.SysFont('Arial', 12)
 
-sim_speed = 1
-FPS=60
-
 # pymunk 0, 0 is in bottom left
 # pygame 0, 0 is in top left
-def mtog(x, y):
-    return int(x), d_height - int(y)
+def mtog(x, y): # TODO: make this take a tuple instead
+    return int(x), WINDOW_HEIGHT - int(y)
+
+def stom(pos):
+    x, y = pos
+    return (WINDOW_WIDTH//2 - TABLE_WIDTH//2 + x, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 + y)
+
 
 class Ball():
-    def __init__(self, x, y, color = (255, 255, 255), draw_stripe = False, draw_label = False, label = ''):
+    def __init__(self, space, pos, color = (255, 255, 255), draw_stripe = False, draw_label = False, label = ''):
+        # body
         self.body = pymunk.Body()
-        self.body.position = x, y
+        self.body.position = pos
 
-        self.shape = pymunk.Circle(self.body, ball_radius)
+        # shape
+        self.shape = pymunk.Circle(self.body, BALL_RADIUS)
         self.shape.density = 25
         self.shape.elasticity = 0.9
         self.shape.collision_type = 1
 
-        self.shape.label = label
-
-        self.color = color
-        self.draw_stripe = draw_stripe
-        self.draw_label = draw_label
+        # custom
+        self.shape.custom = {
+            'label': label,
+            'color': color,
+            'draw_stripe': draw_stripe,
+            'draw_label': draw_label
+        }
 
         space.add(self.body, self.shape)
-        self.text_surface = font.render(self.shape.label, False, (255, 255, 255))
 
-    def draw(self):
+    def step(self):
+        friction_force = (self.body.mass * 9.81) * 0.8
+        dir = -pymunk.Vec2d.normalized(self.body.velocity)
+        self.body.apply_force_at_local_point((friction_force * dir.x, friction_force * dir.y), (0,0))
+
+    def draw(self, display):
         x, y = self.body.position
 
-        pygame.draw.circle(display, (0, 0, 0), mtog(x, y), ball_radius + 1)
-        pygame.draw.circle(display, self.color, mtog(x, y), ball_radius)
+        # circle with outline
+        pygame.draw.circle(display, (0, 0, 0), mtog(x, y), BALL_RADIUS + 1)
+        pygame.draw.circle(display, self.shape.custom['color'], mtog(x, y), BALL_RADIUS)
         
-        if self.draw_stripe:
-            pygame.draw.line(display, (255, 255, 255), mtog(x - ball_radius, y), mtog(x + ball_radius, y), 2)
+        if self.shape.custom['draw_stripe']:
+            pygame.draw.line(display, (255, 255, 255), mtog(x - BALL_RADIUS, y), mtog(x + BALL_RADIUS, y), 2)
 
-        if self.draw_label:
-            text_width, _ = font.size(self.shape.label)
-            display.blit(self.text_surface, mtog(x - text_width//2, y + ball_radius))
+        if self.shape.custom['draw_label']:
+            text_width, _ = font.size(self.shape.custom['label'])
+
+            text_surface = font.render(
+                self.shape.custom['label'],
+                False,
+                (255, 255, 255)
+            )
+
+            display.blit(text_surface, mtog(x - text_width//2, y + BALL_RADIUS))
 
 class Bound():
-    def __init__(self, x0, y0, x1, y1):
-        self.x0 = x0
-        self.y0 = y0
-
-        self.x1 = x1
-        self.y1 = y1
-
+    def __init__(self, space, start, end):
+        # body
         self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
 
-        self.shape = pymunk.Segment(self.body, (x0, y0), (x1, y1), 2.5)
+        # shpae
+        self.shape = pymunk.Segment(self.body, start, end, TRIM_RADIUS)
         self.shape.elasticity = 0.8
 
+        # custom
+        self.shape.custom = {
+            'start': start,
+            'end': end
+        }
+
         space.add(self.body, self.shape)
 
-    def draw(self):
-        pygame.draw.line(display, (84, 57, 29), mtog(self.x0, self.y0), mtog(self.x1, self.y1), 5)
+    def step(self):
+        pass
+
+    def draw(self, display):
+        x0, y0 = self.shape.custom['start']
+        x1, y1 = self.shape.custom['end']
+
+        pygame.draw.line(display, (84, 57, 29), mtog(x0, y0), mtog(x1, y1), TRIM_RADIUS * 2)
 
 class Pocket():
-    def __init__(self, x, y, pos):
+    def __init__(self, space, pos, label):
+        # body
         self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
-        self.body.position = x, y
+        self.body.position = pos
 
-        self.shape = pymunk.Circle(self.body, pocket_radius - ball_radius)
+        # shape
+        self.shape = pymunk.Circle(self.body, POCKET_RADIUS - BALL_RADIUS)
         self.shape.collision_type = 2
-        self.shape.pos = pos
         self.shape.sensor = True
+
+        # custom
+        self.shape.custom = {
+            'label': label
+        }
 
         space.add(self.body, self.shape)
 
-    def draw(self):
+    def step(self):
+        pass
+
+    def draw(self, display):
         x, y = self.body.position
-        pygame.draw.circle(display, (0, 0, 0), mtog(x, y), pocket_radius)
-        #pygame.draw.circle(display, (0, 255, 0), mtog(x, y), pocket_radius - ball_radius) # critical region
+        pygame.draw.circle(display, (0, 0, 0), mtog(x, y), POCKET_RADIUS)
+        #pygame.draw.circle(display, (0, 255, 0), mtog(x, y), POCKET_RADIUS - BALL_RADIUS) # critical region     
 
-def collide(arbiter, space, data):
-    a, b = arbiter.shapes
-    print(a.label, b.pos)
+class Sim():
+    def __init__(self, state, draw=False):
+        self.clock = pygame.time.Clock()
+        self.space = pymunk.Space()
 
-    return True
+        self.draw = draw
+        if draw:
+            self.display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-def sim():
-    balls = [
-        Ball(
-            random.randint(d_width//2 - t_width//2, d_width//2 + t_width//2),
-            random.randint(d_height//2 - t_height//2, d_height//2 + t_height//2), 
-            ball_colors[n],
-            n > 8,
-            True,
-            str(n)
-        ) for n in ball_colors
-    ]
+        self.geometry = {
+            "bounds": [
+                Bound(self.space, (WINDOW_WIDTH//2 - TABLE_WIDTH//2 + TABLE_CORNER, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 + TRIM_RADIUS), (WINDOW_WIDTH//2 - TABLE_MID//2, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 + TRIM_RADIUS)),
+                Bound(self.space, (WINDOW_WIDTH//2 + TABLE_MID//2, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 + TRIM_RADIUS), (WINDOW_WIDTH//2 + TABLE_WIDTH//2 - TABLE_CORNER, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 + TRIM_RADIUS)),
+                Bound(self.space, (WINDOW_WIDTH//2 - TABLE_WIDTH//2 + TABLE_CORNER, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - TRIM_RADIUS), (WINDOW_WIDTH//2 - TABLE_MID//2, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - TRIM_RADIUS)),
+                Bound(self.space, (WINDOW_WIDTH//2 + TABLE_MID//2, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - TRIM_RADIUS), (WINDOW_WIDTH//2 + TABLE_WIDTH//2 - TABLE_CORNER, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - TRIM_RADIUS)),
+                Bound(self.space, (WINDOW_WIDTH//2 - TABLE_WIDTH//2 - TRIM_RADIUS, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 - TABLE_CORNER), (WINDOW_WIDTH//2 - TABLE_WIDTH//2 - TRIM_RADIUS, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 + TABLE_CORNER)),
+                Bound(self.space, (WINDOW_WIDTH//2 + TABLE_WIDTH//2 + TRIM_RADIUS, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 - TABLE_CORNER), (WINDOW_WIDTH//2 + TABLE_WIDTH//2 + TRIM_RADIUS, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 + TABLE_CORNER))
+            ],
+            "pockets": [
+                Pocket(self.space, (WINDOW_WIDTH//2 - TABLE_WIDTH//2, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2), 'Top Left'),
+                Pocket(self.space, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 + POCKET_RADIUS//2), 'Top Middle'),
+                Pocket(self.space, (WINDOW_WIDTH//2 + TABLE_WIDTH//2, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2), 'Top Right'),
+                Pocket(self.space, (WINDOW_WIDTH//2 - TABLE_WIDTH//2, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2), 'Bottom Left'),
+                Pocket(self.space, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - POCKET_RADIUS//2), 'Bottom Middle'),
+                Pocket(self.space, (WINDOW_WIDTH//2 + TABLE_WIDTH//2, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2), 'Bottom Right')
+            ],
+            "balls": [
+                Ball(
+                    self.space,
+                    stom(state[n]), 
+                    BALL_COLORS[n],
+                    n > 8,
+                    True,
+                    str(n)
+                ) for n in BALL_COLORS
+            ],
+            "p1": [
+                Ball(self.space, (random.randint(WINDOW_WIDTH//2 - TABLE_WIDTH//2, WINDOW_WIDTH//2 + TABLE_WIDTH//2), random.randint(WINDOW_HEIGHT//2 - TABLE_HEIGHT//2, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2)), label='p1')
+            ],
+            "p2": [
+                Ball(self.space, (random.randint(WINDOW_WIDTH//2 - TABLE_WIDTH//2, WINDOW_WIDTH//2 + TABLE_WIDTH//2), random.randint(WINDOW_HEIGHT//2 - TABLE_HEIGHT//2, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2)), label='p2')
+            ]
+        }
 
-    p1_cue_ball = Ball(random.randint(d_width//2 - t_width//2, d_width//2 + t_width//2), random.randint(d_height//2 - t_height//2, d_height//2 + t_height//2), label='p1')
-    p2_cue_ball = Ball(random.randint(d_width//2 - t_width//2, d_width//2 + t_width//2), random.randint(d_height//2 - t_height//2, d_height//2 + t_height//2), label='p2')
+        handler = self.space.add_collision_handler(1, 2)
+        handler.begin = Sim.collide
 
-    #balls[0].shape.color = (0, 0, 255)
+    @staticmethod
+    def collide(arbiter, space, data):
+        a, b = arbiter.shapes
+        print(a.custom['label'], b.custom['label'])
 
-    bounds = [Bound(d_width//2 - t_width//2 + t_corner, d_height//2 + t_height//2, d_width//2 - t_mid//2, d_height//2 + t_height//2),
-              Bound(d_width//2 + t_mid//2, d_height//2 + t_height//2, d_width//2 + t_width//2 - t_corner, d_height//2 + t_height//2),
-              
-              Bound(d_width//2 - t_width//2 + t_corner, d_height//2 - t_height//2, d_width//2 - t_mid//2, d_height//2 - t_height//2),
-              Bound(d_width//2 + t_mid//2, d_height//2 - t_height//2, d_width//2 + t_width//2 - t_corner, d_height//2 - t_height//2),
-              
-              Bound(d_width//2 - t_width//2, d_height//2 + t_height//2 - t_corner, d_width//2 - t_width//2, d_height//2 - t_height//2 + t_corner),
-              Bound(d_width//2 + t_width//2, d_height//2 + t_height//2 - t_corner, d_width//2 + t_width//2, d_height//2 - t_height//2 + t_corner)]
-    
-    pockets = [Pocket(d_width//2 - t_width//2, d_height//2 + t_height//2, 'Top Left'),
-               Pocket(d_width//2, d_height//2 + t_height//2 + pocket_radius//2, 'Top Middle'),
-               Pocket(d_width//2 + t_width//2, d_height//2 + t_height//2, 'Top Right'),
-               Pocket(d_width//2 - t_width//2, d_height//2 - t_height//2, 'Bottom Left'),
-               Pocket(d_width//2, d_height//2 - t_height//2 - pocket_radius//2, 'Bottom Middle'),
-               Pocket(d_width//2 + t_width//2, d_height//2 - t_height//2, 'Bottom Right')]
+        return True
 
-    handler = space.add_collision_handler(1, 2)
-    handler.begin = collide
+    def move(self):
+        self.geometry["p1"][0].body.apply_impulse_at_local_point((1000000,0),(0,0))
 
-    p1_cue_ball.body.apply_impulse_at_local_point((1000000,0),(0,0))
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+            if self.draw:
+                self.display.fill((255, 255, 255))
+                pygame.draw.rect(self.display, (0, 0, 0), pygame.Rect(WINDOW_WIDTH//2 - TABLE_WIDTH//2 - TABLE_BEZEL - 5, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - TABLE_BEZEL - 5, TABLE_WIDTH + TABLE_BEZEL * 2 + 10, TABLE_HEIGHT + TABLE_BEZEL * 2 + 10))
+                pygame.draw.rect(self.display, (101, 67, 33), pygame.Rect(WINDOW_WIDTH//2 - TABLE_WIDTH//2 - TABLE_BEZEL, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - TABLE_BEZEL, TABLE_WIDTH + TABLE_BEZEL * 2, TABLE_HEIGHT + TABLE_BEZEL * 2))
+                pygame.draw.rect(self.display, (0, 128, 0), pygame.Rect(WINDOW_WIDTH//2 - TABLE_WIDTH//2, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2, TABLE_WIDTH, TABLE_HEIGHT))
+
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 - TABLE_WIDTH//8, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - TABLE_BEZEL//2), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 - TABLE_WIDTH//4, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - TABLE_BEZEL//2), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 - 3*TABLE_WIDTH//8, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - TABLE_BEZEL//2), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 + TABLE_WIDTH//8, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - TABLE_BEZEL//2), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 + TABLE_WIDTH//4, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - TABLE_BEZEL//2), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 + 3*TABLE_WIDTH//8, WINDOW_HEIGHT//2 - TABLE_HEIGHT//2 - TABLE_BEZEL//2), MARK_RADIUS)
+
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 - TABLE_WIDTH//8, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 + TABLE_BEZEL//2), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 - TABLE_WIDTH//4, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 + TABLE_BEZEL//2), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 - 3*TABLE_WIDTH//8, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 + TABLE_BEZEL//2), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 + TABLE_WIDTH//8, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 + TABLE_BEZEL//2), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 + TABLE_WIDTH//4, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 + TABLE_BEZEL//2), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 + 3*TABLE_WIDTH//8, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2 + TABLE_BEZEL//2), MARK_RADIUS)
+
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 - TABLE_WIDTH//2 - TABLE_BEZEL//2, WINDOW_HEIGHT//2 - TABLE_HEIGHT//4), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 - TABLE_WIDTH//2 - TABLE_BEZEL//2, WINDOW_HEIGHT//2), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 - TABLE_WIDTH//2 - TABLE_BEZEL//2, WINDOW_HEIGHT//2 + TABLE_HEIGHT//4), MARK_RADIUS)
+
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 + TABLE_WIDTH//2 + TABLE_BEZEL//2, WINDOW_HEIGHT//2 - TABLE_HEIGHT//4), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 + TABLE_WIDTH//2 + TABLE_BEZEL//2, WINDOW_HEIGHT//2), MARK_RADIUS)
+                pygame.draw.circle(self.display, (255, 255, 255), (WINDOW_WIDTH//2 + TABLE_WIDTH//2 + TABLE_BEZEL//2, WINDOW_HEIGHT//2 + TABLE_HEIGHT//4), MARK_RADIUS)
+
+                for group in self.geometry:
+                    for obj in self.geometry[group]:
+                        obj.draw(self.display)
+                        pass
+
+                pygame.display.update()
+                self.clock.tick(FPS * SIM_SPEED)
+
+            for _ in range(SIM_SPEED):
+                for group in self.geometry:
+                    for obj in self.geometry[group]:
+                        obj.step()
+                self.space.step(1/(FPS * SIM_SPEED))
+
+
+            delta = False
+            for group in self.geometry:
+                for obj in self.geometry[group]:
+                    if obj.body.velocity.length > 0.25:
+                        delta = True
+                        break
+                if delta:
+                    break
             
-        display.fill((255, 255, 255))
-
-        pygame.draw.rect(display, (0, 0, 0), pygame.Rect(d_width//2 - t_width//2 - bezel - 5, d_height//2 - t_height//2 - bezel - 5, t_width + bezel * 2 + 10, t_height + bezel * 2 + 10))
-        pygame.draw.rect(display, (101, 67, 33), pygame.Rect(d_width//2 - t_width//2 - bezel, d_height//2 - t_height//2 - bezel, t_width + bezel * 2, t_height + bezel * 2))
-        pygame.draw.rect(display, (0, 128, 0), pygame.Rect(d_width//2 - t_width//2, d_height//2 - t_height//2, t_width, t_height))
-
-        [pocket.draw() for pocket in pockets]
-        [bound.draw() for bound in bounds]
-        [ball.draw() for ball in balls]
-
-        p1_cue_ball.draw()
-        p2_cue_ball.draw()
-
-        pygame.display.update()
-        clock.tick(FPS * sim_speed)
-
-        for _ in range(sim_speed):
-            for ball in [p1_cue_ball, p2_cue_ball] + balls:
-
-                friction_force = (ball.body.mass * 9.81) * 0.8
-                dir = -pymunk.Vec2d.normalized(ball.body.velocity)
-                ball.body.apply_force_at_local_point((friction_force * dir.x, friction_force * dir.y), (0,0))
+            if not delta:
+                break
 
 
+#(random.randint(WINDOW_WIDTH//2 - TABLE_WIDTH//2, WINDOW_WIDTH//2 + TABLE_WIDTH//2), random.randint(WINDOW_HEIGHT//2 - TABLE_HEIGHT//2, WINDOW_HEIGHT//2 + TABLE_HEIGHT//2))
 
-            space.step(1/(FPS * sim_speed))
+pos1, pos2 = P1_BOUNDS
+pos3, pos4 = P2_BOUNDS
 
-sim()
+def random_pos(p1):
+    (x0, y0), (x1, y1) = P1_BOUNDS if p1 else P2_BOUNDS
+    return (random.randint(x0, x1), random.randint(y0, y1))
+
+state = {
+    1 : random_pos(True),
+    2 : random_pos(True),
+    3 : random_pos(True),
+    4 : random_pos(True),
+    5 : random_pos(True),
+    6 : random_pos(True),
+    7 : random_pos(True),
+    9 : random_pos(False),
+    10 : random_pos(False),
+    11 : random_pos(False),
+    12 : random_pos(False),
+    13 : random_pos(False),
+    14 : random_pos(False),
+    15 : random_pos(False)
+}
+
+sim = Sim(state, draw=True)
+sim.move()
+
 pygame.quit()
